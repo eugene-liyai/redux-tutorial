5dbe37e6fc4c532526b39ab9bf4896c190e1016f
Andela-eugene <eugene.liyai@andela.com>
2018-02-17 20:58:32 +0300

    [Feat] add example 2 files

---
+++ js/example2.js
@@ -0,0 +1,15 @@
+class Score extends React.Component {
+  render() {
+    <div>
+      <h2>Score board for the {this.props.teamName}</h2>
+      <div>
+        Score: 50 <br/>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  }
+}
+
+let el = document.getElementById('two');
+ReactDOM.render(<Score teamName="Warriors" />, el);
