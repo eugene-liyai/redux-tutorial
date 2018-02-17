d30e9669abd1b9ca961c17735636b29c5b3dba01
Andela-eugene <eugene.liyai@andela.com>
2018-02-17 11:10:23 +0300

    [Feat] add description

--- pureFunctions/pureFunctionExample.js
+++ pureFunctions/pureFunctionExample.js
@@ -8,6 +8,18 @@ function distanceImpureDunction(time) {
 
 // Pure function
 
+/**
+ *
+ * @param time
+ * @param vesselSpeed
+ * @returns {number}
+ *
+ * Characteristics of pure functions
+ *  only access local variables
+ *  No side effects
+ *  given same input, will always yield same output
+ */
+
 function distancePureFunction(time, vesselSpeed) {
   return time * vesselSpeed;
 }
