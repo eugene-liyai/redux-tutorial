0b03c095110aa2a307f42768fd4164774c74b11d
Andela-eugene <eugene.liyai@andela.com>
2018-02-17 21:24:28 +0300

    [Feat] add constructor

--- js/example2.js
+++ js/example2.js
@@ -1,11 +1,24 @@
 class Score extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+    }
+
+    this.incrementScore = this.incrementScore.bind(this);
+  }
+  incrementScore() {
+    this.setState({
+      score: this.state.score++,
+    });
+  }
   render() {
     return(
       <div>
         <h2>Score board for the {this.props.teamName}</h2>
         <div>
-          Score: 50 <br/>
-          <button>+</button>
+          Score: {this.state.score} <br/>
+          <button onClick={this.incrementScore}>+</button>
           <button>-</button>
         </div>
       </div>
