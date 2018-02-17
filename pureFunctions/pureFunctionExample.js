6506fc51c04a3d636f3b4ab0050de22bef3af52f
Andela-eugene <eugene.liyai@andela.com>
2018-02-17 11:08:11 +0300

    [Feat] demonstrate pure and impure functions

---
+++ pureFunctions/pureFunctionExample.js
@@ -0,0 +1,13 @@
+// Impure function
+
+const speed = 20;
+
+function distanceImpureDunction(time) {
+  return time * speed;
+}
+
+// Pure function
+
+function distancePureFunction(time, vesselSpeed) {
+  return time * vesselSpeed;
+}
